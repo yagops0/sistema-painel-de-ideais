dfe27ceb1f68b8d0526a6b9d058cff832f42a155
yagops0 <yagosalvador34@gmail.com>
2025-05-31 02:21:51 -0300

    feat: Iniciando a implementação do service

--- src/repositories/IdeiaRepository.java
+++ src/repositories/IdeiaRepository.java
@@ -6,11 +6,6 @@ import java.util.List;
 
 public interface IdeiaRepository extends Repository<Ideia>{
 
-    void save(Ideia ideia);
-    Ideia findById(int id);
-    List<Ideia> findAll();
-    void deleteById(int id);
-    void deleteAll();
     List<Ideia> findByStatus(String status);
     List<Ideia> findByTipoIdeia(String tipoIdeia);
 }
